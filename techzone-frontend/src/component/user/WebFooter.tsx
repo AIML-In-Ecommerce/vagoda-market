82eb21c0079e360bd6865e3520dbc9da20e4406f
PHoaiLe <lehoaiphuong13042002@gmail.com>
2024-03-15 09:49:16 +0700

    feat: initialize footer of the website

---
+++ techzone-frontend/src/component/user/WebFooter.tsx
@@ -0,0 +1,220 @@
+'use client'
+
+
+interface WebFooterProps
+{
+
+}
+
+const ServiceInfoTypeMockData = 
+[
+    {
+        _id: "sid-01",
+        name: "Hỗ trợ khách hàng",
+        child:
+        [
+            "si-01", "si-02", "si-03", "si-04", "si-05"
+        ]
+    },
+    {
+        _id: "sid-02",
+        name: "Chính sách mua hàng",
+        child:
+        [
+            "si-06", "si-07", "si-08", "si-09", "si-10", "si-11"
+        ]
+    },
+    {
+        _id: "sid-03",
+        name: "Thông tin TechZone",
+        child:
+        [
+            "si-12", "si-13", "si-14", "si-15", "si-16", "si-17", "si-18"
+        ]
+    },
+    {
+        _id: "sid-04",
+        name: "Cộng đồng TechZone",
+        child:
+        [
+            "si-19", "si-20", "si-21", "si-22"
+        ]
+    },
+    {
+        _id: "sid-05",
+        name: "Email liên hệ",
+        child:
+        [
+            "si-23", "si-24", "si-25"
+        ]
+    }
+]
+
+const ServiceInfoMockData = 
+[
+    {
+        _id: "si-01",
+        name: "Thẻ ưu đãi",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-02",
+        name: "Hướng dẫn mua online",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-03",
+        name: "Ưu đãi cho doanh nghiệp",
+        link: "#",
+        icon:""
+    },
+    {
+        _id: "si-04",
+        name: "Chính sách trả góp",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-05",
+        name: "Dịch vụ sửa chữa",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-06",
+        name: "Điều kiện giao dịch chung",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-07",
+        name: "Chính sách bảo hành",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-08",
+        name: "Chính sách đổi trả",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-09",
+        name: "Chính sách thanh toán",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-10",
+        name: "Giao hàng và lắp đặt",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-11",
+        name: "Dịch vụ lắp đặt và nâng cấp",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-12",
+        name: "Giới thiệu TechZone",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-13",
+        name: "Hệ thống cửa hàng",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-14",
+        name: "Trung tâm bảo hành",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-15",
+        name: "Chính sách bảo mật",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-16",
+        name: "Tin công nghệ",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-17",
+        name: "Hỏi đáp",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-18",
+        name: "Tuyển dụng",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-19",
+        name: "Facebook TechZone",
+        link: "#",
+        icon: "facebook.256x256.png"
+    },
+    {
+        _id: "si-20",
+        name: "TechZone Media",
+        link: "#",
+        icon: "youtube.256x256.png"
+    },
+    {
+        _id: "si-21",
+        name: "TechZone gmail",
+        link: "#",
+        icon: "google.256x256.png"
+    },
+    {
+        _id: "si-22",
+        name: "Chăm sóc khách hàng",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-23",
+        name: "Hỗ trợ khách hàng",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-24",
+        name: "Liên hệ hỗ trợ seller",
+        link: "#",
+        icon: ""
+    },
+    {
+        _id: "si-25",
+        name: "Hỗ trợ phát triển",
+        link: "#",
+        icon: ""
+    }
+]
+
+const PaymentMethodMockData = 
+[
+    
+]
+
+export default function WebFooter({}: WebFooterProps)
+{
+
+    return(
+        <>
+
+        </>
+    )
+}
