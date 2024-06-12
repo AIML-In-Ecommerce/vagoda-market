adecfada0c8cb2d8a47017f8665518553296d815
MiwaUS7605 <minhquang9802@gmail.com>
2024-06-12 16:28:33 +0700

    refactor!: change promotion card

--- techzone-frontend/src/component/customer/product/PromotionCardDetail.tsx
+++ techzone-frontend/src/component/customer/product/PromotionCardDetail.tsx
@@ -1,19 +1,20 @@
-import { PromotionType } from "@/model/PromotionType";
+"use client";
+import { formatCurrencyFromValue } from "@/component/user/utils/CurrencyDisplay";
+import { PromotionType } from "@/model/PromotionType"
 import { Button } from "antd";
 import React from "react";
 import { HiOutlineClipboardDocument } from "react-icons/hi2";
 
 interface PromotionDetailProps {
-    item: PromotionType,
-
+    item: PromotionType
 }
 
 export default function PromotionCardDetail(props: PromotionDetailProps) {
     return (
         <React.Fragment>
-            <div className="mt-3 container flex flex-col gap-y-2">
-                <div className="grid bg-slate-50 p-4">
-                    <div className="col-start-1 w-24 self-center font-semibold">Mã</div>
+            <div className="flex flex-col gap-y-2 p-3">
+                <div className="grid grid-cols-2 bg-slate-50 p-4">
+                    <div className="col-start-1 self-center font-semibold">Mã</div>
                     <div className="col-start-2 flex items-center gap-1">
                         <div>{props.item.code}</div>
                         <Button className="text-lg" icon={<HiOutlineClipboardDocument />} onClick={async () => {
@@ -22,13 +23,22 @@ export default function PromotionCardDetail(props: PromotionDetailProps) {
                         }} />
                     </div>
                 </div>
-                <div className="grid p-4 flex">
-                    <div className="col-start-1 w-24 text-slate-30 self-center font-semibold">Hạn sử dụng</div>
-                    <div className="col-start-2">{props.item.expiredDate}</div>
+                <div className="grid grid-cols-2 p-4 flex">
+                    <div className="col-start-1 text-slate-30 self-center font-semibold">Hạn sử dụng</div>
+                    <div className="col-start-2">{props.item.expiredDate ?? "--/--/---- --:--"}</div>
                 </div>
                 <div className="grid bg-slate-50 p-4 flex flex-col">
                     <div className="text-slate-30 col-start-1 font-semibold">Điều kiện</div>
-                    <div className="col-start-1">{props.item.description}</div>
+                    <div className="col-start-1 flex flex-col">
+                        {
+                            ((!props.item.lowerBound) && (!props.item.upperBound)) ? <div>Không có</div> : (
+                                <>
+                                    <div>{props.item.lowerBound ? `Giá trị đơn hàng tối thiểu ${formatCurrencyFromValue({ value: props.item.lowerBound ?? 0 })}` : ""}</div>
+                                    <div>{props.item.upperBound ? `Giảm tối đa ${formatCurrencyFromValue({ value: props.item.upperBound ?? 0 })}` : ""}</div>
+                                </>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
 
